d398e57fdb2ad42b8104b886236b088bc9af2149
Adrian Lobo <alf8aml04@gmail.com>
2020-09-17 00:03:37 -0400

    assignment 2

---
+++ assignment2.c
@@ -0,0 +1,37 @@
+// 1. HEADER COMMENTS
+// Adrian Lobo
+// EEL-2161
+// Sept, 16 2020
+// Assignment 2
+
+// 2. PREPROCESSOR DERIVATIVES
+#define _CRT_SECURE_NO_WARNINGS // supress warnings
+#include <stdio.h> // printf & scanf
+#include <ctype.h> // tolower & toupper
+
+// 3. FUNCTION PROTOTYPES
+void Name(); // Asks name then prints it
+void Number(); // Asks for number then prints it out multiplied by 10
+void Initials(); // Asks initials and changes capitalization
+
+// 4. MAIN FUNCTION
+int main() {
+    Name(); // Name fuction
+    Number(); // Number function
+    Initials(); // Initials function
+    
+    return 0; // Done!
+}
+
+// 5. FUCTION DEFINTIONS
+void Name() {
+    
+}
+
+void Number() {
+
+}
+
+void Initials() {
+
+}
