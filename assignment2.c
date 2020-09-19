2e87b6afb047bb0e3253956c6b8cf70f6c93fbcd
Adrian Lobo <alf8aml04@gmail.com>
2020-09-19 09:42:59 -0400

    fixes to fit directions

--- assignment2.c
+++ assignment2.c
@@ -10,13 +10,12 @@
 #include <ctype.h> // tolower & toupper
 
 // 3. FUNCTION PROTOTYPES
-void Name(); // Asks name then prints it
 void Number(); // Asks for number then prints it out multiplied by 10
 void Initials(); // Asks initials and changes capitalization
 
 // 4. MAIN FUNCTION
 int main() {
-    Name(); // Name fuction
+    printf("\nHello, my name is Adrian"); // Prints my name
     Number(); // Number function
     Initials(); // Initials function
     
@@ -24,19 +23,12 @@ int main() {
 }
 
 // 5. FUCTION DEFINTIONS
-void Name() { // Prompts user for a name, then returns it
-    char name[20];
-    printf("\nGive me a name: ");
-    scanf("%19s", name);
-    printf("\nMy name is %s\n\n", name);
-}
-
 void Number() { // Prompts user for a number, multiplies it by ten the returns the result
     int num, newnum;
     printf("\nEnter an integer: ");
     scanf("%d", &num);
     newnum = num * 10;
-    printf("\nYour number now is %d.\n\n", newnum);
+    printf("\nYour number now is %d.\n", newnum);
 }
 
 void Initials() { // Prompts user for their initials then makes the first one lowercase and the second one uppercase
